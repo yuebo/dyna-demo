ca96a3bad32e4451372888a2b5c5dca0577e1793
yuebo <317728991@qq.com>
2018-05-31 22:14:58 +0800

    first commit

---
+++ src/main/java/com/ifreelight/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.ifreelight.demo;
+
+import com.github.yuebo.DynaFormApplication;
+import org.springframework.boot.SpringApplication;
+
+@DynaFormApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
