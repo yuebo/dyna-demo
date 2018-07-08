be72be8b53e3bff4319037b3342582e6adca9513
yuebo <317728991@qq.com>
2018-07-08 21:35:55 +0800

    updated tests for CRUD service

--- src/test/java/com/ifreelight/demo/DemoApplicationTests.java
+++ src/test/java/com/ifreelight/demo/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.ifreelight.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+    @Autowired
+    UserService userService;
     @Test
     public void contextLoads() {
+        User user=new User();
+        user.setName("admin");
+        List<User> result=userService.findAll(user);
+        Assert.assertNotNull(result);
     }
 
 }
---
+++ src/test/java/com/ifreelight/demo/User.java
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Copyright 2002-2017 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+package com.ifreelight.demo;
+
+import com.github.yuebo.dyna.service.jdbc.Query;
+import com.github.yuebo.dyna.service.jdbc.Table;
+
+/**
+ * User: yuebo
+ * Date: 2018/7/5
+ * Time: 22:04
+ */
+@Table("tbl_user")
+public class User {
+    private String id;
+    @Query
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
